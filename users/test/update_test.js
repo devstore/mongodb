c0d0b4f580486ad2f5f2d89e3a57dcd8563bac3a
Ashutosh Singh <ashugotmail@gmail.com>
2022-03-19 19:04:37 -0700

    Update code added

---
+++ users/test/update_test.js
@@ -0,0 +1,54 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+describe('Updating records', () => {
+  let joe
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' })
+    joe.save().then(() => {
+      done()
+    })
+  })
+
+  it('instance type using set n save', (done) => {
+    joe.set('name', 'Alex')
+    joe
+      .save()
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1)
+        assert(users[0].name === 'Alex')
+        done()
+      })
+  })
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1)
+        assert(users[0].name === 'Alex')
+        done()
+      })
+  }
+
+  it('A model instance can update', (done) => {
+    //To avoid typing same .then code like in above set n save test case, we have achieved the same thing using the
+    // function assertName. This saves typing the code again and again.
+    assertName(joe.update({ name: 'Alex' }), done)
+  })
+
+  it('A model class can update', (done) => {
+    //This find all the records with name=Joe and updtes it with 'Alex'
+    assertName(User.updateMany({ name: 'Joe' }, { name: 'Alex' }), done)
+  })
+
+  it('A model class can update one record', (done) => {
+    assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done)
+  })
+
+  it('A model class can find a record with and Id and update', (done) => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done)
+  })
+})
